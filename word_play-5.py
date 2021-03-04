dc175f959224b2fb163f1a0a11d97b56aecc6d5c
Trevayne <p21twhyte@fairfieldprep.org>
2021-03-04 07:36:30 -0500

    rework

--- word_play-5.py
+++ word_play-5.py
@@ -1,15 +1,26 @@
 # Author: Tow Ms 2/12/21
 
-def is_abecedarian(words, letters):
-    with open("words.txt", "r") as infile:
-        num_of_words = 0
-        contents = infile.readlines()
-        for words in contents:
-            a = words.strip()
-            b = sorted(a)
-            if b == list(words):
-                num_of_words += 1
-            return num_of_words
+def is_abecedarian(words):
+    lst = []
+    for letters in words:
+        lst.append(letters)
+    if lst == sorted(words):
+        return True
+    else:
+        return False
 
 
-print(is_abecedarian("a", "B"))
+with open("words.txt", "r") as infile:
+    num_of_words = 0
+    contents = infile.readlines()
+    for words in contents:
+        lst2 = []
+        alpha = words.strip()
+        for lett in alpha:
+            lst2.append(lett)
+        if lst2 == sorted(alpha):
+            num_of_words += 1 
+print(num_of_words)
+
+
+print(is_abecedarian("abc"))
